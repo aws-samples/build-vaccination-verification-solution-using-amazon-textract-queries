e988f6a84f48af89b4d1527ea74c0e9a3fe86683
yrishab <45221995+yrishab@users.noreply.github.com>
2024-01-18 18:23:22 +0530

    Delete vaccination_verification_solution/cdk.out/asset.35896a31a429d7e80cdecdd32ab580e543a3b40ff9b9c00d6aa10ae1f7538749 directory

--- vaccination_verification_solution/cdk.out/asset.35896a31a429d7e80cdecdd32ab580e543a3b40ff9b9c00d6aa10ae1f7538749/test_sam_local.sh
+++
@@ -1,4 +0,0 @@
-sam build
-sam local invoke -e events/event.json -n env.json
-sam local invoke -e events/event-meta-lending.json -n env-no-meta-lending.json
-sam local invoke -e events/event-meta-lending.json -n env-meta-data-lending.json
